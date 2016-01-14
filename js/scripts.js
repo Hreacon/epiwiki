c1f781c93e81bd4dfda7b4ff24462c0f07027795
cory meyers and Nicholas Jensen-Hay <whippoorwill.in.flight@gmail.com>
2016-01-14 13:09:22 -0800

    add array and code function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-  
+  $('.code').each(function(index, obj) {
+    $(this).text($(this).html());
+  });
+  var terms = [
+    ['Markdown', 'A software tool that converts the plain text formatting to HTML'],
+    ['HTML Display Types', 'CSS Property Display'],
+    ['CSS', 'Cascading Style Sheets, how to style your web page!'],
+    ['Arrays', 'A great way of storing local information!'],
+    ['Attributes', 'Attributes provide additional information about HTML elements.'],
+    ['Floats', 'A CSS property'],
+    ['Box Model', 'A box that wraps around HTML element.'],
+    ['Grid System', ''],
+    ['Responsive Design',''],
+    ['JavaScript Arithmatic', ''],
+    ['JavaScript Functions', ''],
+    ['JavaScript Variables', ''],
+    ['JavaScript Strings', ''],
+    ['JavaScript Methods', ''],
+    ['jQuery', 'A powerful library used to easily select and manipulate the DOM'],
+    ['DOM Manipulation', 'Manipulate the web page with JavaScript'],
+    ['Forms', 'Collect input from the user'],
+    ['Branching', ''],
+    ['Looping', ''],
+    ['Bootstrap', ''],
+    ['Divs and Spans', '']
+  ];
+
 });
